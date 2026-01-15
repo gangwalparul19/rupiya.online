dec9a00abd42c18a39a9129399c7bcf62d766fb4
Parul <gangwalparul19@gmail.com>
2026-01-15 18:31:51 +0530

    Unauthorized user changes

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 // Provides offline support and caching
 
 // CACHE_VERSION is injected by build.js during deployment
-const CACHE_VERSION = '1.2.212';
+const CACHE_VERSION = '1.2.213';
 const CACHE_NAME = `rupiya-v${CACHE_VERSION}`;
 const RUNTIME_CACHE = `rupiya-runtime-v${CACHE_VERSION}`;
 
